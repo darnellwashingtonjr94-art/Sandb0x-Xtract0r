73acfa546ae7bd5ebd21d27fca87259a82ed0b20
Credkellar-Boop <darnellwashingtonjr94@gmail.com>
2026-08-20 20:13:08 -0700

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,17 @@
+#[cfg(feature = "api")]
 pub mod api;
+
+#[cfg(feature = "core")]
 pub mod core;
-pub mod extractors;
-pub mod llm_bot;
+
+#[cfg(feature = "orchestrator")]
 pub mod orchestrator;
+
+#[cfg(feature = "reporting")]
 pub mod reporting;
+
+#[cfg(feature = "sandboxes")]
 pub mod sandboxes;
+
+pub mod llm_bot;
+;
